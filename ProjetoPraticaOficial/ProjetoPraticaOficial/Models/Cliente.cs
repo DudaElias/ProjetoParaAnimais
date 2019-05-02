b4b09d344151ba5729c447e4799192b877725949
LorennaNunes <42344195+LorennaNunes@users.noreply.github.com>
2019-05-02 10:29:52 -0300

    az

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
@@ -19,10 +19,13 @@ namespace ProjetoPraticaOficial.Models
 
         [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
-
+        []
         public string Email { get; set; }
+        []
         public string Cpf { get; set; }
+        []
         public string Senha { get; set; }
+        []
         public string Telefone { get; set; }
     }
 }
