600af033a2a4508650317160d431f97b2d4e8b9c
LorennaNunes <42344195+LorennaNunes@users.noreply.github.com>
2019-05-10 13:39:33 -0300

    btn do CriarCadastroCliente

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
@@ -14,15 +14,15 @@ namespace ProjetoPraticaOficial.Models
 
         }
 
-        [Required]
+       // [Required]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
+        //[Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
 
-        [StringLength(100, MinimumLength = 4)]
-        [Required(ErrorMessage = "Informe o seu email")]// arrumar no css
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        //[StringLength(100, MinimumLength = 4)]
+        //[Required(ErrorMessage = "Informe o seu email")]// arrumar no css
+        //[RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O CPF do usuário é obrigatório", AllowEmptyStrings = false)]// arrumar no css
@@ -30,13 +30,13 @@ namespace ProjetoPraticaOficial.Models
         //ele digitar só número
         public string Cpf { get; set; }
 
-        [Required(ErrorMessage = "A senha é obrigatória", AllowEmptyStrings = false)] // arrumar no css
-        [Display(Name = "Informe a Senha")]
-        [StringLength(10, MinimumLength = 4)]
+        //[Required(ErrorMessage = "A senha é obrigatória", AllowEmptyStrings = false)] // arrumar no css
+       // [Display(Name = "Informe a Senha")]
+        //[StringLength(10, MinimumLength = 4)]
         public string Senha { get; set; }
 
-        [Required(ErrorMessage = "O telefone do usuário é obrigatório", AllowEmptyStrings = false)]// arrumar no css
-        [Display(Name ="Telefone do cliente")]
+        //[Required(ErrorMessage = "O telefone do usuário é obrigatório", AllowEmptyStrings = false)]// arrumar no css
+        //[Display(Name ="Telefone do cliente")]
         //[DisplayFormat(DataFormatString = )]
         //digitar só número por causa da função do css
         public string Telefone { get; set; }
