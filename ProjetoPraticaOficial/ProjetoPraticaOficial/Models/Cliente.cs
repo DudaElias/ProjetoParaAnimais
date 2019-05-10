c58bf5e4cbe5c529c7dce17a8cac88af853f2388
LorennaNunes <42344195+LorennaNunes@users.noreply.github.com>
2019-05-10 11:58:33 -0300

    validação

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Cliente.cs
@@ -19,9 +19,26 @@ namespace ProjetoPraticaOficial.Models
 
         [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
+
+        [StringLength(100, MinimumLength = 4)]
+        [Required(ErrorMessage = "Informe o seu email")]// arrumar no css
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O CPF do usuário é obrigatório", AllowEmptyStrings = false)]// arrumar no css
+        [Display(Name = "Cpf do cliente")]
+        //ele digitar só número
         public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória", AllowEmptyStrings = false)] // arrumar no css
+        [Display(Name = "Informe a Senha")]
+        [StringLength(10, MinimumLength = 4)]
         public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O telefone do usuário é obrigatório", AllowEmptyStrings = false)]// arrumar no css
+        [Display(Name ="Telefone do cliente")]
+        //[DisplayFormat(DataFormatString = )]
+        //digitar só número por causa da função do css
         public string Telefone { get; set; }
     }
 }
