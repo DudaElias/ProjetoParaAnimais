07efca9d954529e09299922fa8f0dd9627b34b3e
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-05-14 10:48:02 -0300

    kkkkkkkk

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Script/Cep.js
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Script/Cep.js
@@ -6,7 +6,13 @@
             contentType: "application/json",
             data: {cep: $("#pesquisa").val()},
             success: function (result) {
-                $("#valor").html("<label> Valor: " + result[0] + "Prazo de entrega:" + result[1] + "</label>")
+                $("#valor").html("<label> Valor: " + result[0] + "Prazo de entrega:" + result[1] + "</label>");
+                var para = document.createElement("input");
+                para.type = 'submit';
+                para.value = 'Finalizar Compra';
+
+                var element = document.getElementById("form-cep");
+                element.appendChild(para);
             },
             error: function (xhr, exception) {
                 alert("Erro ao calcular prazo!");
