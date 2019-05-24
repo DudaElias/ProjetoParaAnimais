c9e1f2c3d0ce4e72bd37b08809ed0768ef703195
LorennaNunes <42344195+LorennaNunes@users.noreply.github.com>
2019-05-24 09:21:28 -0300

    aaa

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Filtro.cs
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Models/Filtro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace ProjetoPraticaOficial.Models
     public class Filtro
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Aniaml obrigatório", AllowEmptyStrings = false)]
         public string Animal { get; set; }
  
     }
