c47d817f95ddbf4b5df9cc560d6014a384de5c37
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-05-29 11:16:02 -0300

    sassdasda

--- ProjetoPraticaOficial/ProjetoPraticaOficial/Script/Cep.js
+++ ProjetoPraticaOficial/ProjetoPraticaOficial/Script/Cep.js
@@ -12,11 +12,13 @@
                 contentType: "application/json",
                 data: { cep: $("#cepC").val() },
                 success: function (result) {
-                    $("#valor").html("<label> Valor: " + result[0] + " Prazo de entrega: " + result[1] + "</label>");
+                    $("#valor").html("<label class='lblValor'> Valor: " + result[0] + "<br>"+ " Prazo de entrega: " + result[1] + " dias úteis" + "</label>");
                     var para = document.createElement("input");
                     para.type = 'submit';
                     para.value = 'Finalizar Compra';
                     para.className = 'btn-compra';
+                    var y = document.createElement("span");
+                    y.textContent = 'Informações finais: ';
                     var x = document.createElement("input");
                     document.getElementById("cepC").style.display = 'none';
                     document.getElementById("btn").style.display = 'none';
@@ -25,11 +27,19 @@
                     x.placeholder = 'Complemento';
                     x.name = "complemento";
                     x.className = 'cepC';
-                    document.getElementById().style.top = 'px';
-                    document.getElementById("form-cep").style.height = '200px';
+                    x.style.position = 'absolute';
+                    y.className = 'y';
+                    x.style.top = '55%';
+                    x.style.left = '23%';
+                    document.getElementById("form-cep").style.height = '400px';
+                    document.getElementById("form-cep").style.top = '75%';
+                    document.getElementById('numero').style.display = 'inline';
+                    document.getElementById('qtd').style.display = 'inline';
+
                     var element = document.getElementById("form-cep");
                     element.appendChild(para);
                     element.appendChild(x);
+                    element.appendChild(y);
                 },
                 error: function (xhr, exception) {
                     alert("Erro");
